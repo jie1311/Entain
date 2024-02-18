7981d82a9c586998dc1020b7b497cd6db360edc8
Jie Zhang / Jay <jiezh1311@gmail.com>
2024-02-19 01:59:00 +1100

    Visible only filter to ListRaces (#1)
    * Added visible only filter to ListRaces
    
    * Added test

---
+++ racing/db/db_test.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"database/sql"
+	"testing"
+
+	"git.neds.sh/matty/entain/racing/proto/racing"
+)
+
+func TestInit(t *testing.T) {
+	racingDB, _ := sql.Open("sqlite3", "./racing.db")
+	racesRepo := NewRacesRepo(racingDB)
+
+	if err := racesRepo.Init(); err != nil {
+		t.Errorf("error when initiating db, err: %s", err.Error())
+	}
+}
+
+func TestList(t *testing.T) {
+	racingDB, _ := sql.Open("sqlite3", "./racing.db")
+	racesRepo := NewRacesRepo(racingDB)
+	if err := racesRepo.Init(); err != nil {
+		t.Errorf("error when initiating db, err: %s", err.Error())
+		return
+	}
+
+	filter := racing.ListRacesRequestFilter{
+		MeetingIds:  []int64{10},
+		VisibleOnly: true,
+	}
+
+	races, err := racesRepo.List(&filter)
+	if err != nil {
+		t.Errorf("error when listing races, err: %s", err.Error())
+		return
+	}
+	for _, race := range races {
+		if race.MeetingId != 10 || race.Visible != true {
+			t.Error("list func didn't apply appropriate filters")
+			return
+		}
+	}
+}
