716237e48252a70a46db850fe088daa450ac6560
Jie <jiezh1311@gmail.com>
2024-02-19 16:20:07 +1100

    added get in racing

--- racing/db/db_test.go
+++ racing/db/db_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"testing"
 	"time"
 
@@ -37,7 +38,7 @@ func TestList(t *testing.T) {
 	for _, race := range races {
 		if (race.AdvertisedStartTime.AsTime().After(time.Now()) && race.Status != racing.Status_OPEN) ||
 			(race.AdvertisedStartTime.AsTime().Before(time.Now()) && race.Status != racing.Status_CLOSED) {
-			t.Errorf("list func failed to populate status")
+			t.Error("list func failed to populate status")
 			return
 		}
 	}
@@ -89,8 +90,34 @@ func TestListWithSorting(t *testing.T) {
 	}
 	for i := 0; i < len(races)-1; i++ {
 		if races[i].Id < races[i+1].Id {
-			t.Errorf("list func failed to apply appropriate sorting")
+			t.Error("list func failed to apply appropriate sorting")
 			return
 		}
+	}
+}
+
+func TestGet(t *testing.T) {
+	racingDB, _ := sql.Open("sqlite3", "./racing.db")
+	racesRepo := NewRacesRepo(racingDB)
+	if err := racesRepo.Init(); err != nil {
+		t.Errorf("error when initiating db, err: %s", err.Error())
+		return
+	}
+	var testId int64 = 96
+
+	race, err := racesRepo.Get(testId)
+	if err != nil {
+		t.Errorf("error when getting a race, err: %s", err.Error())
+		return
+	}
+
+	if race == nil {
+		fmt.Printf("no race with this id (%d) found \n", testId)
+		return
+	}
+
+	if race.Id != testId {
+		t.Errorf("get func failed to retrive a race by ID, expected id: %d, got: %d", testId, race.Id)
+		return
 	}
 }
